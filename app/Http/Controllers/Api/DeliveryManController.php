8bb22941c0f003479ad60bd51894501835b708df
Балгынбек Дуйсен <balginbek.balgin97@gmail.com>
2022-09-23 22:28:21 +0600

    fix delivery men

--- app/Http/Controllers/Api/DeliveryManController.php
+++ app/Http/Controllers/Api/DeliveryManController.php
@@ -16,80 +16,93 @@ class DeliveryManController extends Controller
 {
     public function index()
     {
-        $deliveryMen = User::whereHas('deliveryInfo')->get();
+        $deliveryMen = User::whereHas('deliveryInfo')
+                           ->get()
+        ;
 
         return response([
-            'message' => 'List of Delivery Men',
-            'data' => $deliveryMen->load('deliveryInfo'),
-        ], 200);
+                            'message' => 'List of Delivery Men',
+                            'data'    => $deliveryMen->load('deliveryInfo'),
+                        ], 200);
     }
 
     public function store(StoreDeliveryManRequest $request)
     {
         $user = User::create([
-            'name' => $request->get('name'),
-            'email' => $request->get('email'),
-            'password' => Hash::make($request->get('password')),
-        ]);
+                                 'name'     => $request->get('name'),
+                                 'email'    => $request->get('email'),
+                                 'password' => Hash::make($request->get('password')),
+                             ]);
 
-        $user->roles()->attach(Role::whereName('Courier')->first());
+        $user->roles()
+             ->attach(
+                 Role::where('name', '=', 'Courier')
+                     ->first()
+             )
+        ;
 
         DeliveryMan::create([
-            'user_id' => $user->id,
-            'code' => $request->get('code'),
-            'shipping_cost' => $request->get('shipping_cost'),
-            'default' => false,
-        ]);
+                                'user_id'       => $user->id,
+                                'code'          => $request->get('code'),
+                                'shipping_cost' => $request->get('shipping_cost'),
+                                'default'       => false,
+                            ]);
 
         return response([
-            'message' => 'Delivery Man created successfully',
-            'data' => $user->load('deliveryInfo')
-        ], 200);
+                            'message' => 'Delivery Man created successfully',
+                            'data'    => $user->load('deliveryInfo'),
+                        ], 200);
     }
 
     public function show(DeliveryMan $deliveryMan)
     {
-        $deliveryMan = User::find($deliveryMan->user_id)->load('deliveryInfo');
+        $deliveryMan = User::find($deliveryMan->user_id)
+                           ->load('deliveryInfo')
+        ;
 
         return response([
-            'message' => 'Delivery Man info',
-            'data' => $deliveryMan,
-        ], 200);
+                            'message' => 'Delivery Man info',
+                            'data'    => $deliveryMan,
+                        ], 200);
     }
 
     public function update(UpdateDeliveryManRequest $request, DeliveryMan $deliveryMan)
     {
         $deliveryMan->update([
-            'code' => $request->get('code'),
-            'shipping_cost' => $request->get('shipping_cost'),
-        ]);
+                                 'code'          => $request->get('code'),
+                                 'shipping_cost' => $request->get('shipping_cost'),
+                             ]);
 
         $user = User::find($deliveryMan->user_id);
 
         $user->update([
-            'email' => $request->get('email'),
-            'name' => $request->get('name'),
-            'active' => $request->get('active'),
-        ]);
+                          'email'  => $request->get('email'),
+                          'name'   => $request->get('name'),
+                          'active' => $request->get('active'),
+                      ]);
 
         return response([
-            'message' => 'Delivery Man updated successfully',
-            'data' => $user->load('deliveryInfo')
-        ], 200);
+                            'message' => 'Delivery Man updated successfully',
+                            'data'    => $user->load('deliveryInfo'),
+                        ], 200);
     }
 
     public function toggleDefault(ToggleDefaultDeliveryManRequest $request, $id)
     {
         if ($request->get('default')) {
-            DB::table('delivery_men')->update(['default' => false]);
+            DB::table('delivery_men')
+              ->update(['default' => false])
+            ;
         }
 
-        $deliveryMan = DeliveryMan::where('user_id', $id)->first();
+        $deliveryMan = DeliveryMan::where('user_id', $id)
+                                  ->first()
+        ;
 
         $deliveryMan->update(['default' => $request->get('default')]);
 
         return response([
-            'message' => 'Default toggled successfully',
-        ], 200);
+                            'message' => 'Default toggled successfully',
+                        ], 200);
     }
 }
